e78f0aacc3f4084eacaa3a0321500cd27716a59c
yuqizhang <yuqizhang247@gmail.com>
2025-12-26 20:38:48 -0500

    Create Docking Pipline

--- docking_verify/__init__.py
+++ docking_verify/__init__.py
@@ -3,3 +3,32 @@
 # @Author : Yuqi Zhang
 # @Email : yzhan135@kent.edu
 # @File:__init__.py.py
+
+# docking_verify/__init__.py
+from __future__ import annotations
+
+# Core components
+from .embedder import KabschFragmentEmbedder
+from .pdbqt_preparer import PDBQTPreparer, ResidueID
+from .box_builder import LigandCenteredBoxBuilder, DockingBox
+from .vina_runner import VinaDockingRunner, VinaParams, VinaPoseRow
+from .pipeline import DockingPipeline, PipelineConfig, BoxConfig
+
+__all__ = [
+    # embed
+    "KabschFragmentEmbedder",
+    # pdbqt
+    "PDBQTPreparer",
+    "ResidueID",
+    # box
+    "LigandCenteredBoxBuilder",
+    "DockingBox",
+    # vina
+    "VinaDockingRunner",
+    "VinaParams",
+    "VinaPoseRow",
+    # pipeline
+    "DockingPipeline",
+    "PipelineConfig",
+    "BoxConfig",
+]
